952767ac51b2a3aae2f900d7f62464a62dd5de22
AbhiDevWorks <devworksabhinab@gmail.com>
2020-01-31 01:59:09 +0530

    Hardcoded data with class cmps

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react'
 import './App.css'
 import Navbar from './components/layout/Navbar'
-import UserItem from './components/users/UserItem'
+import Users from './components/users/Users'
 
 class App extends Component {
     render() {
-        /*const name = 'John Doe'
-        const loading = false
-        const showName = true*/
-
         return (
-            /*Expressions & conditionals
-            <div className="App">
-                <h1>Hello {name.toUpperCase()}</h1>
-                {loading ? (
-                    <h2>Loading...</h2>
-                ) : (
-                    <h3>Hello Hey there {showName && name}</h3>
-                )}
-            </div>*/
-
             <div className="App">
                 <Navbar />
-                <UserItem></UserItem>
+                <div className="container">
+                    <Users />
+                </div>
             </div>
         )
     }
