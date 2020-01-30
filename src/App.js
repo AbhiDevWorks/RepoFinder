d26ba347364f3e322a214f0322dc864d8de896dc
AbhiDevWorks <devworksabhinab@gmail.com>
2020-01-30 20:54:38 +0530

    Conditionals & Expressions

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
-import React, { Fragment, Component } from 'react'
+import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
     render() {
+        const name = 'John Doe'
+        const loading = false
+        const showName = true
+
         return (
-            <div>
-                {/*Option1*/}
-                <React.Fragment>
-                    <h1>Hello from React 1</h1>
-                </React.Fragment>
-                {/*Option2*/}
-                <Fragment>
-                    <h1>Hello from React 2</h1>
-                </Fragment>
-                {/*Option3*/}
-                <>
-                    <h1>Hello from React 3</h1>
-                </>
-                {/*      <div className="App">
-      <h1>Hello from React</h1>
-  </div>  */}
+            /*Expressions & conditionals*/
+            <div className="App">
+                <h1>Hello {name.toUpperCase()}</h1>
+                {loading ? (
+                    <h2>Loading...</h2>
+                ) : (
+                    <h3>Hello Hey there {showName && name}</h3>
+                )}
             </div>
         )
-
-        /*return React.createElement(
-      "div",
-      { className: "App" },
-      React.createElement("h1", null, "Hello from React")
-    );*/
     }
 }
 
