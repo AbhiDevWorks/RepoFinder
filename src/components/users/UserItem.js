4df8dd544ada11e1286b839d8c61ae0db4ac792a
AbhiDevWorks <devworksabhinab@gmail.com>
2020-01-31 14:35:16 +0530

    Installed React Router, Added Routes in App Cmp, new async methods aded; Created Repos & RepoItem Cmp, created User Cmp to show data on click of More btn;State mgmt without Redux & context Part 2

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
-    //With fn cmps, this keyword is not used. Instead props are passed in the parameter of the fn cmp
+    //With fn cmps, this is not used. Instead props are passed in the parameter of the fn cmp
     //const { login, avatar_url, html_url } = props.user;
 
     return (
@@ -15,9 +16,12 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
             />
             <h3>{login}</h3>
             <div>
-                <a href={html_url} className="btn btn-dark btn-sm my-1">
+                <Link
+                    to={`/user/${login}`}
+                    className="btn btn-dark btn-sm my-1"
+                >
                     More
-                </a>
+                </Link>
             </div>
         </div>
     )
